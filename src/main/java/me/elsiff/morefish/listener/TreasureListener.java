d3fbd844ea9b21664e37b56cc37d1e6773371866
Faceguy <kattreinkyle@gmail.com>
2022-12-28 18:31:03 -0500

    Chests openable in adventure mode

--- src/main/java/me/elsiff/morefish/listener/TreasureListener.java
+++ src/main/java/me/elsiff/morefish/listener/TreasureListener.java
@@ -8,7 +8,9 @@ import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -16,15 +18,23 @@ import org.bukkit.inventory.meta.ItemMeta;
 public class TreasureListener implements Listener {
 
   @EventHandler(priority = EventPriority.LOWEST)
-  public void onPlaceTreasure(BlockPlaceEvent event) {
-    if (event.getBlock().getType() == Material.BLACK_SHULKER_BOX) {
-      ItemMeta meta = event.getItemInHand().getItemMeta();
+  public void onPlaceTreasure(PlayerInteractEvent event) {
+    if (event.getHand() != EquipmentSlot.HAND) {
+      return;
+    }
+    if (event.getAction() != Action.RIGHT_CLICK_AIR &&
+        event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+      return;
+    }
+    ItemStack stack = event.getPlayer().getEquipment().getItemInMainHand();
+    if (stack.getType() == Material.BLACK_SHULKER_BOX) {
+      ItemMeta meta = stack.getItemMeta();
       if (meta != null && meta.getCustomModelData() == 1337) {
         event.setCancelled(true);
-        event.getItemInHand().setAmount(event.getItemInHand().getAmount() - 1);
+        stack.setAmount(stack.getAmount() - 1);
         BlockStateMeta im = (BlockStateMeta) meta;
         ShulkerBox shulker = (ShulkerBox) im.getBlockState();
-        Location location = event.getBlock().getLocation().clone().add(0.5, 0.5, 0.5);
+        Location location = event.getPlayer().getLocation().clone();
         location.getWorld().playSound(location, Sound.AMBIENT_UNDERWATER_ENTER, 1, 1);
         location.getWorld().playSound(location, Sound.BLOCK_ENDER_CHEST_OPEN, 1, 1);
         for (ItemStack i : shulker.getInventory()) {
